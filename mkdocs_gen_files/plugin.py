9733c1d7cad2767c5ba4e58251f0e0c16c35c867
Oleh Prypin <oleh@pryp.in>
2020-12-10 01:50:59 +0100

    Change tmp dir name

--- mkdocs_gen_files/plugin.py
+++ mkdocs_gen_files/plugin.py
@@ -13,7 +13,7 @@ class GenFilesPlugin(mkdocs.plugins.BasePlugin):
     config_scheme = (("scripts", mkdocs.config.config_options.Type(list)),)
 
     def on_files(self, files: mkdocs.structure.files.Files, config: mkdocs.config.Config):
-        self._dir = tempfile.TemporaryDirectory("-mkdocs_gen_files")
+        self._dir = tempfile.TemporaryDirectory(prefix="mkdocs_gen_files_")
 
         with editor.FilesEditor(files, config, self._dir.name) as ed:
             for file_name in self.config["scripts"]:
