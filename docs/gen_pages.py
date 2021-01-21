4a5a756c2ea532b5611bc27af6c0193b13fa1112
Oleh Prypin <oleh@pryp.in>
2021-01-21 03:16:02 +0100

    Make example more interesting

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -4,4 +4,4 @@ for total in range(19, 100, 20):
     with mkdocs_gen_files.open(f"sample/{total}-bottles.md", "w") as f:
         for i in reversed(range(1, total + 1)):
             print(f"{i} bottles of beer on the wall, {i} bottles of beer  ", file=f)
-            print(f"Take one down and pass it around, {i-1} bottles of beer on the wall\n", file=f)
+            print(f"Take one down and pass it around, **{i-1}** bottles of beer on the wall\n", file=f)
