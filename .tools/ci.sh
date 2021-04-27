a614fb416aabefc1cc97fc30c0034fe5c50eea92
Oleh Prypin <oleh@pryp.in>
2021-04-27 23:00:26 +0200

    Upgrade pytype

--- .tools/ci.sh
+++ .tools/ci.sh
@@ -11,5 +11,5 @@ with_groups() {
 "$@" autoflake -i -r --remove-all-unused-imports --remove-unused-variables mkdocs_gen_files
 "$@" isort -q mkdocs_gen_files
 "$@" black -q mkdocs_gen_files
-python -c 'import sys, os; sys.exit((3,8) <= sys.version_info < (3,9) and os.name == "posix")' ||
+python -c 'import sys, os; sys.exit((3,8) <= sys.version_info < (3,10) and os.name == "posix")' ||
 "$@" pytype mkdocs_gen_files
