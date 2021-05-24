823d1b327c128b1bf673be61a0dac4bd60765d8d
Oleh Prypin <oleh@pryp.in>
2021-05-24 15:23:58 +0200

    Rework test setup

--- tests/test_nav.py
+++ tests/test_nav.py
@@ -3,54 +3,8 @@ import pytest
 from mkdocs_gen_files import Nav
 
 
-@pytest.fixture
-def golden(request, golden):
-    return golden.open(f"nav/{request.node.name}.yml")
-
-
-def test_nav_empty(golden):
+@pytest.mark.golden_test("nav/*.yml")
+def test_nav(golden):
     nav = Nav()
+    exec(golden["code"], locals())
     assert "".join(nav.build_literate_nav()) == golden.out["output"]
-
-
-def test_nav_basic(golden):
-    nav = Nav()
-    nav["Hello"] = "test.md"
-    assert "".join(nav.build_literate_nav()) == golden.out["output"]
-
-
-def test_nav_with_index_item(golden):
-    nav = Nav()
-    nav["Hello", "World"] = "hello/world.md"
-    nav["Hello"] = "hello/index.md"
-    assert "".join(nav.build_literate_nav()) == golden.out["output"]
-
-
-def test_nav_chaotic(golden):
-    nav = Nav()
-    nav["Hello", "World"] = "hello/world.md"
-    nav["Hello"] = "hello/index.md"
-    nav["a"] = "b/c/d/a.md"
-    nav["b/c/d/a"] = "a.md"
-    nav["Hello", "Beautiful", "World"] = "hello/world.md"
-    assert "".join(nav.build_literate_nav()) == golden.out["output"]
-
-
-def test_nav_special_chars(golden):
-    nav = Nav()
-    nav["__init__"] = "a.md"
-    nav["__init__.py"] = "a.md"
-    nav["`hi`"] = "a.md"
-    assert "".join(nav.build_literate_nav()) == golden.out["output"]
-
-
-def test_errors():
-    nav = Nav()
-    with pytest.raises(ValueError):
-        nav[()] = "index.md"
-    with pytest.raises(ValueError):
-        nav[""] = "index.md"
-    with pytest.raises(TypeError):
-        nav[5] = "index.md"
-    with pytest.raises(TypeError):
-        nav["a", 5] = "index.md"
