051473f5209e09262fcfba3f978b095f5db0fbe8
Tim Vink <vinktim@gmail.com>
2022-11-28 20:15:00 +0100

    Add generated_by attribute to page.file

--- mkdocs_gen_files/editor.py
+++ mkdocs_gen_files/editor.py
@@ -45,6 +45,7 @@ class FilesEditor:
             dest_dir=self.config["site_dir"],
             use_directory_urls=self.config["use_directory_urls"],
         )
+        new_f.generated_by = "mkdocs-gen-files"
         normname = pathlib.PurePath(name).as_posix()
 
         if new or normname not in self._files:
